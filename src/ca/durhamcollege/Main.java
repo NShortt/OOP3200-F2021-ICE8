6476dd62939e6989964685c6aa5e4c41755778d8
Nicholas Shortt <nicholas.shortt@hotmail.com>
2021-11-10 22:36:55 -0500

    Include basic input and output test

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,21 @@
+/**
+ * @author Nicholas Shortt
+ * @studentID 100630003
+ * @date November 10, 2021
+ */
 package ca.durhamcollege;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String name = null;
+        Scanner keyboard =new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You entered: %s", name);
     }
 }
